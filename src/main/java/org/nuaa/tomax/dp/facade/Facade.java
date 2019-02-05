3d1474c9b40fc9960a0286b9ff74ea98112eb83e
tomax <1121584497@qq.com>
2019-02-05 21:16:22 +0800

    src/main/java/org/nuaa/tomax/dp/facade/Facade.java

--- src/main/java/org/nuaa/tomax/dp/facade/Facade.java
+++ src/main/java/org/nuaa/tomax/dp/facade/Facade.java
@@ -7,11 +7,19 @@ package org.nuaa.tomax.dp.facade;
  * @date 2019/2/5
  */
 public class Facade {
+    private SubSystemA subSystemA;
+    private SubSystemB subSystemB;
+
+    public Facade(SubSystemA subSystemA, SubSystemB subSystemB) {
+        this.subSystemA = subSystemA;
+        this.subSystemB = subSystemB;
+    }
+
     void method() {
         System.out.println("facade");
         System.out.println("other methods ...");
-        new SubSystemA().methodA();
-        new SubSystemB().methodB();
+        subSystemA.methodA();
+        subSystemB.methodB();
         System.out.println("other methods ...");
     }
 }
